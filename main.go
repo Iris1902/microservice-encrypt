8473d0c84cf014d64c7c4f1e3da3b558230bc6a2
Iris1902 <irispicis1962@gmail.com>
2025-06-27 09:57:10 -0500

    :wrench: fix(main.go): correct health check response message

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ type VerifyResponse struct {
 // Health check handler
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OKEY"))
+	w.Write([]byte("OK"))
 }
 
 // HashPassword using bcrypt
